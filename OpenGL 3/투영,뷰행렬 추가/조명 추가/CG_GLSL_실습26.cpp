6687b51aeb5b3e1ba65de5d1c0695c2c13be4082
Tkadha(home) <emforhs228@naver.com>
2023-11-18 21:27:27 +0900

    end26

--- OpenGL 3/투영,뷰행렬 추가/조명 추가/CG_GLSL_실습26.cpp
+++ OpenGL 3/투영,뷰행렬 추가/조명 추가/CG_GLSL_실습26.cpp
@@ -10,9 +10,9 @@
 #include <random>
 #include <fstream>
 
-
 #define Width 1200
 #define Height 800
+#define side_length 0.2
 #define PI 3.14159265
 
 void make_vertexShaders();
@@ -23,10 +23,15 @@ GLvoid Reshape(int w, int h);
 char* filetobuf(const char* file);
 void InitBuffer();
 GLvoid Keyboard(unsigned char key, int x, int y);
-void SpecialKeyboard(int, int, int);
-void rotate_x(int);
+
 void rotate_y(int);
+void rotate_z(int);
 void rotate_light(int);
+
+GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
+GLuint vertexShader, fragmentShader; //--- 세이더 객체
+GLuint shaderProgramID;
+GLuint vao;
 
 std::random_device rd;
 std::mt19937 gen(rd());
@@ -76,23 +81,23 @@ public:
 				++f_incount;
 			}
 		}
-		for (int i = 0; i < vertex_count/3; i++)
+		for (int i = 0; i < vertex_count / 3; i++)
 		{
 			glm::vec3 normal = glm::cross(vertexdata[i * 3 + 1] - vertexdata[i * 3 + 0], vertexdata[i * 3 + 2] - vertexdata[i * 3 + 0]);
 			normaldata[i * 3 + 0] = normal;
 			normaldata[i * 3 + 1] = normal;
 			normaldata[i * 3 + 2] = normal;
-
 		}
-		for (int i = 0; i < vertex_count; ++i) {
-			colordata[color_count].x = random_color(gen);
-			colordata[color_count].y = random_color(gen);
-			colordata[color_count].z = random_color(gen);
-			color_count++;
+		colordata[0].x = random_color(gen);
+		colordata[0].y = random_color(gen);
+		colordata[0].z = random_color(gen);
+		for (int i = 1; i < vertex_count; ++i) {
+			colordata[i].x = colordata[0].x;
+			colordata[i].y = colordata[0].y;
+			colordata[i].z = colordata[0].z;
 		}
 
 		glGenBuffers(3, vbo);
-
 		glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
 		glBufferData(GL_ARRAY_BUFFER, vertex_count * sizeof(glm::vec3), vertexdata, GL_STATIC_DRAW);
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
@@ -122,36 +127,32 @@ public:
 
 };
 
-GLchar* vertexSource, * fragmentSource; //--- 소스코드 저장 변수
-GLuint vertexShader, fragmentShader; //--- 세이더 객체
-GLuint shaderProgramID;
-GLuint vao;
-
-
-Shape cube;
-Shape tetra;
-
+bool perspec_on;
+bool solid_line;
 float rail[200][3];
+float subrail[200][3];
+int planet_point[3];
+int sub_planet_point[3];
 
+float light_rail[200][3];
 
-bool cube_draw;
-bool tetra_draw;
-bool timer_x;
-bool timer_y;
-float x_1;
-float y_1;
-float light_rotate_y;
-float addx_1;
-float addy_1;
 float movex;
 float movey;
-bool light_on;
+float movez;
+float rotatey;
+float rotatez;
+bool rotatey_on;
+bool rotatez_on;
+GLUquadricObj* planet;
 
+Shape cube;
+Shape sphere;
+
+int light_count;
 glm::vec3 light_pos;
 glm::vec3 light_box_pos;
 glm::vec3 light_color;
-
-GLUquadricObj* way;
+float light_rotate_y;
 bool rotate_light_on;
 
 void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
@@ -165,42 +166,51 @@ void main(int argc, char** argv) //--- 윈도우 출력하고 콜백함수 설정
 	glewExperimental = GL_TRUE;
 	glewInit();
 	make_shaderProgram();
-	x_1 = 0.0f;
-	y_1 = 0.0f;
-	light_box_pos.x = light_pos.x = 0.f;
-	light_box_pos.y = light_pos.y = 0.f;
-	light_box_pos.z = light_pos.z = 1.f;
-	light_color.x = 1.0f;
-	light_color.y = 1.0f;
-	light_color.z = 1.0f;
-	cube_draw = true;
-	tetra_draw = false;
-
+	//std::cout << sizeof(rail) << std::endl;
 	float x;
 	float z;
 	float angle;
 	for (int i = 0; i < 200; ++i) {
-		angle = (i + 50) * 1.8f;
-		x = cos(angle * PI / 180) * light_pos.z;
-		z = sin(angle * PI / 180) * light_pos.z;
+		angle = i * 1.8f;
+		x = cos(angle * PI / 180) * 1.0f;
+		z = sin(angle * PI / 180) * 1.0f;
 		rail[i][0] = x;
 		rail[i][1] = 0.0f;
 		rail[i][2] = z;
+		x = cos(angle * PI / 180) * 0.4f;
+		z = sin(angle * PI / 180) * 0.4f;
+		subrail[i][0] = x;
+		subrail[i][1] = 0.0f;
+		subrail[i][2] = z;
+	}
+	light_color.x = 1.0f;
+	light_color.y = 0.f;
+	light_color.z = 0.f;
+	light_box_pos.x = light_pos.x = 0.f;
+	light_box_pos.y = light_pos.y = 0.f;
+	light_box_pos.z = light_pos.z = 1.f;
 
+	for (int i = 0; i < 200; ++i) {
+		angle = (i + 50) * 1.8f;
+		x = cos(angle * PI / 180) * light_pos.z;
+		z = sin(angle * PI / 180) * light_pos.z;
+		light_rail[i][0] = x;
+		light_rail[i][1] = 0.0f;
+		light_rail[i][2] = z;
 	}
 	InitBuffer();
+	
 	//--- 세이더 읽어와서 세이더 프로그램 만들기
 	glutDisplayFunc(drawScene); //--- 출력 콜백 함수
 	glutReshapeFunc(Reshape);
 	glutKeyboardFunc(Keyboard);
-	glutSpecialFunc(SpecialKeyboard);
 	glutMainLoop();
 }
 
 GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 {
 	GLfloat rColor, gColor, bColor;
-	rColor = gColor = bColor = 0.3f;
+	rColor = gColor = bColor = 0.2f;
 	glClearColor(rColor, gColor, bColor, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -214,9 +224,12 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 	glEnableVertexAttribArray(ColorLocation);
 	glEnableVertexAttribArray(NormalLocation);
 
-
 	unsigned int proj_location = glGetUniformLocation(shaderProgramID, "projection");
 	unsigned int view_location = glGetUniformLocation(shaderProgramID, "view");
+	unsigned int shape_location = glGetUniformLocation(shaderProgramID, "transform");
+	unsigned int color_location = glGetUniformLocation(shaderProgramID, "setcolor");
+
+
 
 	//투영행렬
 	glm::mat4 proj = glm::mat4(1.0f);
@@ -233,8 +246,6 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 	//view = glm::rotate(view, glm::radians(30.f), glm::vec3(0.f, 1.f, 0.f));
 	glUniformMatrix4fv(view_location, 1, GL_FALSE, &view[0][0]);
 
-
-
 	unsigned int lightPosLocation = glGetUniformLocation(shaderProgramID, "lightPos");
 	glUniform3f(lightPosLocation, light_pos.x, light_pos.y, light_pos.z);
 	unsigned int lightColorLocation = glGetUniformLocation(shaderProgramID, "lightColor");
@@ -242,49 +253,70 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 	unsigned int viewPosLocation = glGetUniformLocation(shaderProgramID, "viewPos");
 	glUniform3f(viewPosLocation, cameraPos.x, cameraPos.y, cameraPos.z);
 
-	glFrontFace(GL_CCW);
+
+
 	glm::mat4 trans = glm::mat4(1.0f);
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_CULL_FACE);
-	unsigned int trans_location = glGetUniformLocation(shaderProgramID, "transform");
+	trans = glm::translate(trans, glm::vec3(movex, movey, movez));
+	trans = glm::rotate(trans, glm::radians(rotatez), glm::vec3(0.0f, 0.0f, 1.0f));
+	trans = glm::rotate(trans, glm::radians(rotatey), glm::vec3(0.0f, 1.0f, 0.0f));
+	trans = glm::rotate(trans, glm::radians(10.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	trans = glm::rotate(trans, glm::radians(10.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+	gluQuadricDrawStyle(planet, GLU_LINE);
+
+	glBindBuffer(GL_ARRAY_BUFFER, sphere.vbo[0]);
+	glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, sphere.vbo[1]);
+	glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, sphere.vbo[2]);
+	glVertexAttribPointer(NormalLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
 	for (int i = 0; i < 200; ++i) {
 		glm::mat4 rail1 = trans;
 		rail1 = glm::translate(rail1, glm::vec3(rail[i][0], rail[i][1], rail[i][2]));
-		glUniformMatrix4fv(trans_location, 1, GL_FALSE, glm::value_ptr(rail1));
-		gluSphere(way, 0.005, 5, 5);
-	}
-	if (tetra_draw) {
-		trans = glm::rotate(trans, glm::radians(x_1), glm::vec3(1.0f, 0.0f, 0.0f));
-		trans = glm::rotate(trans, glm::radians(y_1), glm::vec3(0.0f, 1.0f, 0.0f));	
-		trans = glm::translate(trans, glm::vec3(0.0f, -0.5f, 0.0f));
-		trans = glm::scale(trans, glm::vec3(0.7, 0.7, 0.7));
-		unsigned int trans_location = glGetUniformLocation(shaderProgramID, "transform");
-		glUniformMatrix4fv(trans_location, 1, GL_FALSE, glm::value_ptr(trans));
-		glBindBuffer(GL_ARRAY_BUFFER, tetra.vbo[0]);
-		glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-		glBindBuffer(GL_ARRAY_BUFFER, tetra.vbo[1]);
-		glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-		glBindBuffer(GL_ARRAY_BUFFER, tetra.vbo[2]);
-		glVertexAttribPointer(NormalLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-		glDrawArrays(GL_TRIANGLES, 0, tetra.vertex_count);
+		glUniformMatrix4fv(shape_location, 1, GL_FALSE, glm::value_ptr(rail1));
+		gluSphere(planet, 0.005, 5, 5);
+		glm::mat4 rail2 = trans;
 
-	}
-	else {
-		trans = glm::translate(trans, glm::vec3(movex, movey, 0.0f));
-		trans = glm::rotate(trans, glm::radians(x_1), glm::vec3(1.0f, 0.0f, 0.0f));
-		trans = glm::rotate(trans, glm::radians(y_1), glm::vec3(0.0f, 1.0f, 0.0f));
-		trans = glm::translate(trans, glm::vec3(-0.5f, -0.5f, -0.5f));
-		unsigned int trans_location = glGetUniformLocation(shaderProgramID, "transform");
-		glUniformMatrix4fv(trans_location, 1, GL_FALSE, glm::value_ptr(trans));
-		glBindBuffer(GL_ARRAY_BUFFER, cube.vbo[0]);
-		glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-		glBindBuffer(GL_ARRAY_BUFFER, cube.vbo[1]);
-		glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-		glBindBuffer(GL_ARRAY_BUFFER, cube.vbo[2]);
-		glVertexAttribPointer(NormalLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-		glDrawArrays(GL_TRIANGLES, 0, cube.vertex_count);
+		glm::mat4 sub_rail1 = trans;
+		sub_rail1 = glm::translate(sub_rail1, glm::vec3(subrail[i][0], subrail[i][1], subrail[i][2]));
+		sub_rail1 = glm::translate(sub_rail1, glm::vec3(rail[planet_point[0]][0], rail[planet_point[0]][1], rail[planet_point[0]][2]));
+		glUniformMatrix4fv(shape_location, 1, GL_FALSE, glm::value_ptr(sub_rail1));
+		gluSphere(planet, 0.005, 5, 5);
+
+		glm::mat4 light_rail1 = trans;
+		light_rail1 = glm::translate(light_rail1, glm::vec3(light_rail[i][0], light_rail[i][1], light_rail[i][2]));
+		glUniformMatrix4fv(shape_location, 1, GL_FALSE, glm::value_ptr(light_rail1));
+		gluSphere(planet, 0.005, 5, 5);
 	}
 
+
+	gluQuadricDrawStyle(planet, GLU_FILL);
+	trans = glm::scale(trans, glm::vec3(0.5f, 0.5f, 0.5f));
+	glUniformMatrix4fv(shape_location, 1, GL_FALSE, glm::value_ptr(trans));
+	glDrawArrays(GL_TRIANGLES, 0, sphere.vertex_count);
+
+	glm::mat4 planet1 = trans;
+	planet1 = glm::translate(planet1, glm::vec3(rail[planet_point[0]][0], rail[planet_point[0]][1], rail[planet_point[0]][2]));
+	planet1 = glm::translate(planet1, glm::vec3(rail[planet_point[0]][0], rail[planet_point[0]][1], rail[planet_point[0]][2]));
+	planet1 = glm::scale(planet1, glm::vec3(0.5f, 0.5f, 0.5f));
+	glUniformMatrix4fv(shape_location, 1, GL_FALSE, glm::value_ptr(planet1));
+	glDrawArrays(GL_TRIANGLES, 0, sphere.vertex_count);
+
+	glm::mat4 sub_planet1 = planet1;
+	sub_planet1 = glm::translate(sub_planet1, glm::vec3(subrail[sub_planet_point[0]][0], subrail[sub_planet_point[0]][1], subrail[sub_planet_point[0]][2]));
+	sub_planet1 = glm::translate(sub_planet1, glm::vec3(subrail[sub_planet_point[0]][0], subrail[sub_planet_point[0]][1], subrail[sub_planet_point[0]][2]));
+	sub_planet1 = glm::translate(sub_planet1, glm::vec3(subrail[sub_planet_point[0]][0], subrail[sub_planet_point[0]][1], subrail[sub_planet_point[0]][2]));
+	sub_planet1 = glm::translate(sub_planet1, glm::vec3(subrail[sub_planet_point[0]][0], subrail[sub_planet_point[0]][1], subrail[sub_planet_point[0]][2]));
+	sub_planet1 = glm::scale(sub_planet1, glm::vec3(0.5f, 0.5f, 0.5f));
+	glUniformMatrix4fv(shape_location, 1, GL_FALSE, glm::value_ptr(sub_planet1));
+	glDrawArrays(GL_TRIANGLES, 0, sphere.vertex_count);
+
+
+
+
+	//glBindBuffer(GL_ARRAY_BUFFER, railvbo);
+	//glVertexAttribPointer(PosLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
+	//glDrawArrays(GL_LINE_STRIP, 0, 100);
 
 	glm::mat4 light_box = glm::mat4(1.0f);
 	light_box = glm::rotate(light_box, glm::radians(light_rotate_y), glm::vec3(0.0f, 1.0f, 0.0f));
@@ -299,7 +331,8 @@ GLvoid drawScene() //--- 콜백 함수: 그리기 콜백 함수
 	glVertexAttribPointer(ColorLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
 	glBindBuffer(GL_ARRAY_BUFFER, cube.vbo[2]);
 	glVertexAttribPointer(NormalLocation, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), 0);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
+	glDrawArrays(GL_TRIANGLES, 0, cube.vertex_count);
+
 
 	glDisableVertexAttribArray(ColorLocation);
 	glDisableVertexAttribArray(PosLocation);
@@ -387,68 +420,73 @@ char* filetobuf(const char* file)
 void InitBuffer() {
 
 	glGenVertexArrays(1, &vao);
-
 	glBindVertexArray(vao);
-	cube.load_Obj("./cube.obj");
-	tetra.load_Obj("./tetra.obj");
 
-	way = gluNewQuadric();
-	gluQuadricNormals(way, GLU_SMOOTH);
-	gluQuadricOrientation(way, GLU_OUTSIDE);
-	gluQuadricTexture(way, GL_FALSE);
+	cube.load_Obj("cube.obj");
+	sphere.load_Obj("sphere.obj");
 
+	planet = gluNewQuadric();
+	gluQuadricNormals(planet, GLU_SMOOTH);
+	gluQuadricOrientation(planet, GLU_OUTSIDE);
+	gluQuadricTexture(planet, GL_FALSE);
 }
 GLvoid Keyboard(unsigned char key, int x, int y)
 {
-	float light_x;
-	float light_z;
-	float angle;
-
 	switch (key) {
-	case 'n':
-		if (cube_draw) {
-			cube_draw = false;
-			tetra_draw = true;
-		}
-		else {
-			cube_draw = true;
-			tetra_draw = false;
-		}
-		break;
-	case 'm':
-		if (light_on)
-		{
-			light_color.x = 0.f;
-			light_color.y = 0.f;
-			light_color.z = 0.f;
-			light_on = false;
-		}
-		else
-		{
+
+	case 'c':
+		light_color = { 0.f,0.f,0.f };
+		if (light_count == 0) {
 			light_color.x = 1.f;
+		}
+		else if (light_count == 1) {
 			light_color.y = 1.f;
+		}
+		else if (light_count == 2) {
 			light_color.z = 1.f;
-			light_on = true;
 		}
+		++light_count;
+		if (light_count > 2)
+			light_count = 0;
 		break;
+	case 'w':
+		movey += 0.1f;
+		break;
+	case 's':
+		movey -= 0.1f;
+		break;
+
+	case 'a':
+		movex -= 0.1f;
+		break;
+	case 'd':
+		movex += 0.1f;
+		break;
+
+	case '+':
+		movez += 0.1f;
+		break;
+	case '-':
+		movez -= 0.1f;
+		break;
+
 	case 'y':
-		addy_1 = 1.0f;
-		if (!timer_y) {
-			timer_y = true;
-			glutTimerFunc(50, rotate_y, 0);
-		}
+	case 'Y':
+		if (rotatey_on)
+			rotatey_on = false;
 		else {
-			timer_y = false;
+			rotatey_on = true;
+			glutTimerFunc(10, rotate_y, 0);
 		}
 		break;
-	case 'Y':
-		addy_1 = -1.0f;
-		if (!timer_y) {
-			timer_y = true;
-			glutTimerFunc(50, rotate_y, 0);
-		}
+
+	case 'z':
+	case 'Z':
+		if (rotatez_on)
+			rotatez_on = false;
 		else {
-			timer_y = false;
+			rotatez_on = true;
+			glutTimerFunc(10, rotate_z, 0);
 		}
 		break;
 	case 'r':
@@ -460,31 +498,6 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 		else
 			rotate_light_on = false;
 		break;
-	case 'z':
-		light_pos.z -= 0.3f;
-		light_box_pos.z -= 0.3f;
-		for (int i = 0; i < 200; ++i) {
-			angle = (i + 50) * 1.8f;
-			light_x = cos(angle * PI / 180) * light_box_pos.z;
-			light_z = sin(angle * PI / 180) * light_box_pos.z;
-			rail[i][0] = light_x;
-			rail[i][1] = 0.0f;
-			rail[i][2] = light_z;
-
-		}
-		break;
-	case 'Z':
-		light_pos.z += 0.3f;
-		light_box_pos.z += 0.3f;
-		for (int i = 0; i < 200; ++i) {
-			angle = (i + 50) * 1.8f;
-			light_x = cos(angle * PI / 180) * light_box_pos.z;
-			light_z = sin(angle * PI / 180) * light_box_pos.z;
-			rail[i][0] = light_x;
-			rail[i][1] = 0.0f;
-			rail[i][2] = light_z;
-		}
-		break;
 	case 'q':
 	case 'Q':
 		exit(0);
@@ -492,38 +505,20 @@ GLvoid Keyboard(unsigned char key, int x, int y)
 	}
 	glutPostRedisplay();
 }
-void rotate_x(int value){
-	x_1 += addx_1;
-	if (timer_x)
-		glutTimerFunc(1, rotate_x, value);
-	glutPostRedisplay();
-}
 void rotate_y(int value) {
-	y_1 += addy_1;
-	if (timer_y)
-		glutTimerFunc(1, rotate_y, value);
+	++rotatey;
+	if (rotatey_on)
+		glutTimerFunc(10, rotate_y, 0);
 	glutPostRedisplay();
-}
-void SpecialKeyboard(int key, int x, int y) {
 
-	switch (key) {
-	case GLUT_KEY_LEFT:
-		movex -= 0.05f;
-		break;
-	case GLUT_KEY_RIGHT:
-		movex += 0.05f;
-		break;
-	case GLUT_KEY_UP:
-		movey += 0.05f;
-		break;
-	case GLUT_KEY_DOWN:
-		movey -= 0.05f;
-		break;
-	}
+}
+void rotate_z(int value) {
+	++rotatez;
+	if (rotatez_on)
+		glutTimerFunc(10, rotate_z, 0);
 	glutPostRedisplay();
-}
 
-int light_count;
+}
 void rotate_light(int value)
 {
 	light_pos.x = rail[light_count][0];
